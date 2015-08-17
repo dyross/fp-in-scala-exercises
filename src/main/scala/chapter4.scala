b9a87335a8297988a3c04be5a08b633b98decc1f
David Y. Ross <dyross@gmail.com>
2015-08-16 17:20:35 -0700

    Initial option methods.

---
+++ src/main/scala/chapter4.scala
@@ -0,0 +1,32 @@
+package chapter4
+
+object ex_4_1 {
+  sealed trait Option[+A] {
+    def map[B](f: A => B): Option[B] = this match {
+      case Some(a) => Some(f(a))
+      case None => None
+    }
+
+    def flatMap[B](f: A => Option[B]): Option[B] = {
+      this map f getOrElse None
+    }
+
+    def getOrElse[B >: A](default: => B): B = this match {
+      case Some(a) => a
+      case None => default
+    }
+
+    def orElse[B >: A](ob: => Option[B]): Option[B] = {
+      this map Some.apply getOrElse ob
+    }
+
+    def filter(f: A => Boolean): Option[A] = {
+      this flatMap { a =>
+        if (f(a)) Some(a)
+        else None
+      }
+    }
+  }
+  case class Some[+A](value: A) extends Option[A]
+  case object None extends Option[Nothing]
+}
