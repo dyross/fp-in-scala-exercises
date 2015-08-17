9da523c54e7273a670a5bacf875d172d1a6c4811
David Y. Ross <dyross@gmail.com>
2015-08-16 17:28:59 -0700

    Implement variance.

--- src/main/scala/chapter4.scala
+++ src/main/scala/chapter4.scala
@@ -30,3 +30,14 @@ object ex_4_1 {
   case class Some[+A](value: A) extends Option[A]
   case object None extends Option[Nothing]
 }
+
+object ex_4_2 {
+  def mean(xs: Seq[Double]): Option[Double] =
+    if (xs.isEmpty) None
+    else Some(xs.sum / xs.length)
+
+  def variance(xs: Seq[Double]): Option[Double] =
+    mean(xs) flatMap { m =>
+      mean(xs map (x => math.pow(x - m, 2)))
+    }
+}
