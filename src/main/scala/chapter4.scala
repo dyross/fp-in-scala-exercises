ab23eb148867809fb706bfda30a523a5d98bb14c
David Y. Ross <dyross@gmail.com>
2015-08-18 11:05:34 -0700

    A few more Option methods.

--- src/main/scala/chapter4.scala
+++ src/main/scala/chapter4.scala
@@ -41,3 +41,49 @@ object ex_4_2 {
       mean(xs map (x => math.pow(x - m, 2)))
     }
 }
+
+object ex_4_3 {
+  def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = {
+    a flatMap (aa => b map (bb => f(aa, bb)))
+  }
+}
+
+object ex_4_4 {
+  def sequence[A](a: List[Option[A]]): Option[List[A]] = {
+    @scala.annotation.tailrec
+    def go(l: List[Option[A]], accum: List[A]): Option[List[A]] = {
+      l match {
+        case Nil => Some(accum.reverse)
+        case Some(h) :: t => go(t, h :: accum)
+        case None :: _ => None
+      }
+    }
+
+    go(a, Nil)
+  }
+}
+
+object ex_4_5 {
+  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = {
+    val builder = scala.collection.mutable.ListBuffer.empty[B]
+    @scala.annotation.tailrec
+    def go(l: List[A]): Option[List[B]] = {
+      l match {
+        case Nil =>
+          Some(builder.toList)
+        case h :: t =>
+          f(h) match {
+            case Some(value) =>
+              builder += value
+              go(t)
+            case None => None
+          }
+      }
+    }
+
+    go(a)
+  }
+
+  def sequenceViaTraverse[A](a: List[Option[A]]): Option[List[A]] =
+    traverse(a)(x => x)
+}
